23c3e25e0524c313e51b7378d3032f04af039455
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-11-12 17:41:03 -0500

    Private Route and Login works

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 import './App.css';
+import Login from './components/Login';
+import AuthFriends from './components/AuthFriends';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<div>
+					<h2>
+						<Link to='/login'>Login</Link>
+					</h2>
+					<h2>
+						<Link to='/private'>Private Page</Link>
+					</h2>
+				</div>
+				<Switch>
+					<PrivateRoute exact path='/private' component={AuthFriends} />
+					<Route path='/login' component={Login} />
+					<Route component={Login} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
